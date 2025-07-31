718f771a584d9b538c1d42e5eee3fe97c5e3fb8b
Mako Kamiya <mako4kamiya@gmail.com>
2025-07-31 18:18:56 +0900

    カバーフローのパターンを追加し、フロントページのヘッダーとフッターのタグを修正。ビジュアルポートフォリオの構造を更新。

---
+++ patterns/coverflow.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: coverflow
+ * Slug: mako-kamiya-original-portfolio-theme/coverflow
+ * Categories: 
+ */
+?>
+<!-- wp:group {"metadata":{"patternName":"core/block/569","name":"coverflow","categories":["gallery"]},"className":"visual-portfolio","layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical","verticalAlignment":"center","justifyContent":"stretch"}} -->
+<div class="wp-block-group visual-portfolio"><!-- wp:visual-portfolio/block {"block_id":"Z13giDe","align":"wide","content_source":"post-based","posts_offset":0,"layout":"slider","slider_effect":"coverflow","slider_autoplay_hover_pause":true,"slider_items_height_static":640,"slider_items_height_dynamic":100,"slider_loop":true,"slider_arrows":false,"slider_bullets":true,"slider_mousewheel":true,"items_gap":0,"layout_elements":{"top":{"elements":[],"align":"left"},"items":{"elements":["items"]},"bottom":{"elements":[],"align":"center"}},"custom_css":"selector%20%7B%0A%7D"} /--></div>
+<!-- /wp:group -->
