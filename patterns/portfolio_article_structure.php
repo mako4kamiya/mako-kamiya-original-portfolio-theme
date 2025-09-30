d7d55fe1ce3561bfcff3d8f5a84b65ff3c0cd64f
Mako Kamiya <mako4kamiya@gmail.com>
2025-09-30 21:57:20 +0900

    ポートフォリオについての説明を新規作成し、制作の目的、デザインのポイント、技術的な特徴、制作プロセス、成果と学び、今後の展望を含むコンテンツを追加しました。

---
+++ patterns/portfolio_article_structure.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Title: ポートフォリオについての説明
+ * Slug: mako-kamiya-original-portfolio-theme/portfolio_article_structure
+ * Categories: 
+ */
+?>
+<!-- wp:group {"metadata":{"name":"ポートフォリオについての説明"},"style":{"spacing":{"blockGap":"2rem","padding":{"right":"2rem","left":"2rem","top":"3rem","bottom":"3rem"}}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="padding-top:3rem;padding-right:2rem;padding-bottom:3rem;padding-left:2rem"><!-- wp:group {"metadata":{"name":"見出し＋リスト"},"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.5"}},"textColor":"custom-text-primary-dark","fontSize":"custom-4","fontFamily":"noto-sans"} -->
+<h2 class="wp-block-heading has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-4-font-size" style="font-style:normal;font-weight:700;line-height:1.5">制作の目的</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<li class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">スキルや経験を分かりやすく整理する</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>多様なスキルを見やすくまとめる</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<li class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">学んできたことをひと目で伝える</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"見出し＋文章"},"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.5"}},"textColor":"custom-text-primary-dark","fontSize":"custom-4","fontFamily":"noto-sans"} -->
+<h2 class="wp-block-heading has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-4-font-size" style="font-style:normal;font-weight:700;line-height:1.5">デザインのポイント</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<p class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">「優しさ」「親しみやすさ」「誠実さ」を表現するため、余白を活かしたシンプルなデザインにしました。制作物を引き立てるニュートラルな配色を採用し、フォントは英語に軽やかで視認性の高い「Work Sans」、日本語に安心感のある「Noto Sans JP」を使用しています。</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"見出し＋リスト"},"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.5"}},"textColor":"custom-text-primary-dark","fontSize":"custom-4","fontFamily":"noto-sans"} -->
+<h2 class="wp-block-heading has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-4-font-size" style="font-style:normal;font-weight:700;line-height:1.5">技術的な特徴</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<li class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">サーバー契約、独自ドメイン取得、WordPressテーマの設置、サイト公開までの一連の運用</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>WordPress プラグイン「Create Block Theme」を活用した、WordPress オリジナルブロックテーマの開発</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>WordPress プラグイン「Visual Portfolio, Posts &amp; Image Gallery（Gutenbergブロック対応）」を活用した制作物の表示</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>WordPress プラグイン「Secure Custom Fields (SCF)」を活用したコンテンツ管理</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<li class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">「GitHub Actions」を活用したデプロイの自動化</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"見出し＋番号リスト"},"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.5"}},"textColor":"custom-text-primary-dark","fontSize":"custom-4","fontFamily":"noto-sans"} -->
+<h2 class="wp-block-heading has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-4-font-size" style="font-style:normal;font-weight:700;line-height:1.5">制作プロセス</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list {"ordered":true} -->
+<ol class="wp-block-list"><!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<li class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">企画と要件定義：ポートフォリオに必要な要素と目標を明確化</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>デザイン制作：Figma で画面構造とデザインの作成</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>WordPress実装：サイトエディター、プラグインを活用したテーマ開発と機能実装</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>コーディング：カスタムCSS によるスタイリング</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>テストと最適化：クロスブラウザテスト、レスポンシブ対応、表示速度の最適化</li>
+<!-- /wp:list-item --></ol>
+<!-- /wp:list --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"見出し＋文章"},"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.5"}},"textColor":"custom-text-primary-dark","fontSize":"custom-4","fontFamily":"noto-sans"} -->
+<h2 class="wp-block-heading has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-4-font-size" style="font-style:normal;font-weight:700;line-height:1.5">成果と学び</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<p class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">このプロジェクトを通じて、デザインからコーディング、WordPress実装、サイト公開までの一貫した制作フローを経験し、自身のスキルを向上させることができました。 </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<p class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">当初はクラシックテーマで制作しましたが、フルサイト編集に対応したブロックテーマが主流であることを学び、作り直しました。この過程で、ブロックテーマの仕組みを実践的に理解することができました。</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"見出し＋リスト"},"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.5"}},"textColor":"custom-text-primary-dark","fontSize":"custom-4","fontFamily":"noto-sans"} -->
+<h2 class="wp-block-heading has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-4-font-size" style="font-style:normal;font-weight:700;line-height:1.5">今後の展望</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-text-primary-dark"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.7"}},"textColor":"custom-text-primary-dark","fontSize":"custom-8","fontFamily":"noto-sans"} -->
+<li class="has-custom-text-primary-dark-color has-text-color has-link-color has-noto-sans-font-family has-custom-8-font-size" style="font-style:normal;font-weight:400;line-height:1.7">制作物の追加</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>ブログ機能を追加し、制作過程や学びを発信</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
