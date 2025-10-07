e62a39a5e770aa0ee9110712314058eab8d43a13
Mako Kamiya <mako4kamiya@gmail.com>
2025-10-07 15:28:58 +0900

    app.jsのGSAPアニメーション用のスクリプトを追加し、custom.jsを削除しました。また、functions.phpにおいてGSAPライブラリをエンキューする関数を定義しました。

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
 console.log("app.js is loaded");
+
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function(event){
+
+    console.log("DOM loaded");
+
+    //wait until images, links, fonts, stylesheets, and js is loaded
+    window.addEventListener("load", function(e){
+
+        //custom GSAP code goes here
+        // This tween will rotate an element with a class of .my-element
+        // gsap.to('.my-element', {
+        //     rotation: 360,
+        //     duration: 2,
+        //     ease: 'bounce.out'
+        // })
+
+        console.log("window loaded");
+    }, false);
+
+});
--- functions.php
+++ functions.php
@@ -5,11 +5,22 @@ if (!function_exists('mkopt_block_assets')) :
             'mkopt-custom',
             get_theme_file_uri('assets/css/custom.css')
         );
-        wp_enqueue_script (
-            'mkopt-custom',
-            get_theme_file_uri('assets/js/custom.js')
-        );
+        // wp_enqueue_script (
+        //     'mkopt-custom',
+        //     get_theme_file_uri('assets/js/custom.js')
+        // );
     }
 endif;
 add_action('enqueue_block_assets', 'mkopt_block_assets');
 
+// GSAPの読み込み
+function theme_gsap_script(){
+    // The core GSAP library
+    wp_enqueue_script( 'gsap-js', 'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js', array(), false, true );
+    // ScrollTrigger - with gsap.js passed as a dependency
+    wp_enqueue_script( 'gsap-st', 'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollTrigger.min.js', array('gsap-js'), false, true );
+    // Your animation code file - with gsap.js passed as a dependency
+    wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . '/assets/js/app.js', array('gsap-js'), false, true );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
