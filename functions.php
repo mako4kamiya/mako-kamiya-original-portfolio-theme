2e19e4b8dc5d9a75774fd776ba10dbf1cf4bbb87
Mako Kamiya <mako4kamiya@gmail.com>
2025-10-07 14:43:25 +0900

    カスタムスクリプトを追加し、ブロックアセットのスタイルとスクリプトを適切にエンキューする関数を定義しました。

--- functions.php
+++ functions.php
@@ -1,10 +1,14 @@
 <?php
-if (!function_exists('mkopt_enqueue_styles')) :
-    function mkopt_enqueue_styles() {
-        wp_enqueue_style(
+if (!function_exists('mkopt_block_assets')) :
+    function mkopt_block_assets () {
+        wp_enqueue_style (
             'mkopt-custom',
             get_theme_file_uri('assets/css/custom.css')
         );
+        wp_enqueue_script (
+            'mkopt-custom',
+            get_theme_file_uri('assets/js/custom.js')
+        );
     }
 endif;
-add_action('enqueue_block_assets', 'mkopt_enqueue_styles');
+add_action('enqueue_block_assets', 'mkopt_block_assets');
