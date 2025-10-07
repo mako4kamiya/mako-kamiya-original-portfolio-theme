7271eede707e8cfd3f98752aed657e8443b499d2
Mako Kamiya <mako4kamiya@gmail.com>
2025-10-07 17:38:10 +0900

    app.jsのデバッグ用のconsole.logをコメントアウトし、GSAPアニメーションのターゲットを配列として処理するように変更しました。

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,29 @@
-console.log("app.js is loaded");
+// console.log("app.js is loaded");
 
 // wait until DOM is ready
 document.addEventListener("DOMContentLoaded", function(event){
 
-    console.log("DOM loaded");
+    // console.log("DOM loaded");
 
     //wait until images, links, fonts, stylesheets, and js is loaded
     window.addEventListener("load", function(e){
 
         //custom GSAP code goes here
-        gsap.to('.inview', {
-            scrollTrigger: {
-                trigger: '.inview',
-                start: 'top center',
-                end: 'bottom center',
-                markers: true,
-                toggleActions: 'play reverse play reverse',
-            },
-            opacity: 1,
-        })
+        let targets = gsap.utils.toArray(".inview");
+        targets.forEach((target) => {
+            gsap.to(target, {
+                scrollTrigger: {
+                    trigger: target,
+                    start: "top center",
+                    end: "bottom center",
+                    toggleActions: "play reverse play reverse",
+                    // markers: true,
+                },
+                opacity: 1,
+            });
+        });
 
-        console.log("window loaded");
+        // console.log("window loaded");
     }, false);
 
 });
